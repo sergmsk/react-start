8a7ba02f613ba945c12d95a201b7b3bcf6fe92d7
KS <me@gmail.com>
2017-09-26 17:48:22 +0300

    add Article List

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Article from './Article.js';
+import ArticleList from './ArticleList';
 import {articles} from './fixtures.js';
 
-const  article = articles[0];
+//const  article = articles[0];
 
-render(<Article article = {article}/>, document.getElementById('container'));
+render(<ArticleList articles = {articles}/>, document.getElementById('container'));
