300b23a161007039e9cd6b985d367c7e7fdc34c2
Sergey <noreplygithub@yandex.ru>
2017-09-27 13:53:37 +0300

    Добавлена проверка отсутствия комментариев в компоненте ComentList. В этом случае компонента возвращает "Комментариев нет"

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function CommentList (comments){
-
+    if(!comments.comments) return <div>Комментариев нет</div>
     const commentsElement = comments.comments.map((comment) =>
         <li key={comment.id}><h4>{comment.user}</h4>{comment.text}</li> );
 
