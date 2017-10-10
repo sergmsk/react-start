f5aa1f3cfd7ca20d2c8198499c7329a97f52a3f5
Sergey <noreplygithub@yandex.ru>
2017-10-10 13:52:59 +0300

    повторное слияние с веткой курса

--- src/Article.js
+++ src/Article.js
@@ -1,52 +1,45 @@
-import React, {Component} from 'react';
-import Comments from './Comments';
+import React, {Component} from 'react'
+import CommentList from './CommentList'
 
 export default class Article extends Component {
-    constructor(props){
-        super(props);
+    constructor(props) {
+        super(props)
+
         this.state = {
-            isOpen:false
+            isOpen: true
         }
     }
 
-    render(){
-        const {article} = this.props;
-        const {isOpen} = this.state;
+    render() {
+        const {article} = this.props
+        const {isOpen} = this.state
         return (
             <div>
                 <h3>{article.title}</h3>
-                <button onClick={this.toggleOpen}>
-                    {isOpen?'close':'open'}
+                <button onClick = {this.toggleOpen}>
+                    {isOpen ? 'close' : 'open'}
                 </button>
                 {this.getBody()}
             </div>
         )
     }
-    getBody(){
-        if(!this.state.isOpen) return null;
-        const {article} = this.props;
+
+    getBody() {
+        if (!this.state.isOpen) return null
+        const {article} = this.props
         return (
-            <div>
-                <section>{article.text}</section>
-                <Comments comments={article.comments}/>
-            </div>)
+            <section>
+               {article.text}
+               <CommentList comments = {article.comments}/>
+            </section>
+        )
     }
-    toggleOpen=()=>{
+
+    toggleOpen = (ev) => {
+        ev.preventDefault()
+        console.log('---', ev.nativeEvent)
         this.setState({
-            isOpen:!this.state.isOpen
+            isOpen: !this.state.isOpen
         })
     }
 }
-
-/*
- export default function Article(props){
-     //console.log('---', props);
-     const {article} = props;
-     return (
-         <div>
-             <h3>{article.title}</h3>
-             <section>{article.text}</section>
-         </div>
-     )
- }
-*/
