a81a7a745e471b612d6dc0eafb6bb7e8b1a65221
KS <me@gmail.com>
2017-10-10 12:30:29 +0300

    Слияние с веткой курса

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import Article from './Article';
+import React from 'react'
+import Article from './Article'
 
+export default function ArticleList({articles}) {
+    const articleElements = articles.map(article => <li key = {article.id}><Article article = {article}/></li>)
 
-export default function ArticleList({articles}){
-    //console.log(props);
-    //const articles = props.articles;
-    const articleElements = articles.map((article) => <li key={article.id}><Article article={article}/></li>)
     return (
         <ul>
             {articleElements}
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Comment from './Comment'
 
-export default function CommentList (comments){
-    if(!comments.comments) return <div>Комментариев нет</div>
-    const commentsElement = comments.comments.map((comment) =>
-        <li key={comment.id}><h4>{comment.user}</h4>{comment.text}</li> );
+class CommentList extends Component {
+    state = {
+        isOpen: false
+    }
 
-    return (
-        <ul>
-            {commentsElement}
-        </ul>
-    )
+    render() {
+        const text = this.state.isOpen ? 'hide comments' : 'show comments'
+        return (
+            <div>
+                <button onClick = {this.toggleOpen}>{text}</button>
+                {this.getBody()}
+            </div>
+        )
+    }
+
+    getBody() {
+        if (!this.state.isOpen) return null
+
+        const {comments} = this.props
+        if (!comments || !comments.length) return <p>No comments yet</p>
+
+        return (
+            <ul>
+                {comments.map(comment => <li key = {comment.id}><Comment comment = {comment} /></li>)}
+            </ul>
+        )
+    }
+
+    toggleOpen = ev => this.setState({
+        isOpen: !this.state.isOpen
+    })
 }
+
+export default CommentList
