39c2e95e30825b813c1352f3efb3d37ce196fb5d
KS <me@gmail.com>
2017-10-10 16:19:22 +0300

    добавляем в проект поддержку prop-types

--- src/Article.js
+++ src/Article.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import CommentList from './CommentList'
 
 export default class Article extends Component {
@@ -6,7 +7,7 @@ export default class Article extends Component {
         super(props)
 
         this.state = {
-            isOpen: false
+            isOpen: true
         }
     }
 
