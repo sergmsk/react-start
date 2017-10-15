db2b1b0445640c5e6a3f219c755641df89019158
Sergey <noreplygithub@yandex.ru>
2017-10-15 20:01:17 +0300

    слияние с веткой курса

---
+++ src/components/Article.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import CommentList from './CommentList'
+import toggleOpen from '../decorators/toggleOpen'
+
+class Article extends Component {
+    static propTypes = {
+        article:PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            text: PropTypes.string
+        }).isRequired
+    }
+
+
+    render() {
+        const {article, isOpen,toggleOpen} = this.props
+
+        return (
+            <div>
+                <h3>{article.title}</h3>
+                <button onClick = {toggleOpen}>
+                    {isOpen ? 'close' : 'open'}
+                </button>
+                {this.getBody()}
+            </div>
+        )
+    }
+
+    getBody() {
+        //if (!this.state.isOpen) return null
+        const {article,isOpen} = this.props
+        if (!isOpen) return null
+        return (
+            <section>
+               {article.text}
+               <CommentList comments = {article.comments}/>
+            </section>
+        )
+    }
+
+
+}
+
+export default toggleOpen(Article);
---
+++ src/components/CommentList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Comment from './Comment'
+import toggleOpen from '../decorators/toggleOpen'
+
+class CommentList extends Component {
+    //задаем дефолтные пропсы, используются, если в них из родителя ничего не приходит
+    static defaultProps ={
+        comments:[]
+    }
+
+
+    render() {
+        const text = this.props.isOpen ? 'hide comments' : 'show comments'
+        return (
+            <div>
+                <button onClick = {this.props.toggleOpen}>{text}</button>
+                {this.getBody()}
+            </div>
+        )
+    }
+
+    getBody() {
+        const {comments, isOpen} = this.props
+        if (!isOpen) return null
+        if (!comments.length) return <p>No comments yet</p>
+
+        return (
+            <ul>
+                {comments.map(comment => <li key = {comment.id}><Comment comment = {comment} /></li>)}
+            </ul>
+        )
+    }
+
+
+}
+
+export default toggleOpen(CommentList);
