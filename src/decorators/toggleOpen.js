c89352b144874a4eda29078555b58deedb551f6a
KS <me@gmail.com>
2017-10-15 18:37:09 +0300

    обернули компонент Article в декоратор toggleOpen (отвечает за раскрытие статьи)

--- src/decorators/toggleOpen.js
+++ src/decorators/toggleOpen.js
@@ -1,7 +1,17 @@
 import React, {Component as ReactComponent} from 'react'
 
 export default (OriginalComponent) => class WrappedComponent extends ReactComponent {
+    state = {
+        isOpen:false
+    }
     render() {
-        return <OriginalComponent {...this.props}/>
+        return <OriginalComponent {...this.props} {...this.state} toggleOpen={this.toggleOpen}/>
+    }
+    toggleOpen = (ev) => {
+        ev && ev.preventDefault && ev.preventDefault()
+        //console.log('---', ev.nativeEvent)
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     }
 }
